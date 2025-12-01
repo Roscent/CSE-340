9fa0dd7e02e1bf162f5205757d302fe81a28bffd
roscent <ofu24001@byui.edu>
2025-12-01 15:57:27 +0100

    week 04

---
+++ routes/accountRoute.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities/")
+
+// Route to build login view
+router.get("/", utilities.handleErrors(accountController.buildLogin))
+// The path for the 'My Account' link is typically /account/login. 
+// When mounted at /account in server.js, the router handles /login.
+// We will use the explicit /login route here to clarify the path.
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Route to build registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+module.exports = router;
