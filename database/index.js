dbc22aeb425f389ffef4f31c47d40ae42ee90abc
roscent <ofu24001@byui.edu>
2025-12-05 17:16:31 +0100

    ch

--- database/index.js
+++ database/index.js
@@ -1,37 +1,35 @@
-const { Pool } = require("pg")
-require("dotenv").config()
+const { Pool } = require("pg");
+require("dotenv").config();
+
 /* ***************
  * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
  * *************** */
-let pool
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-})
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
+let config = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+// Add SSL for development environments
+if (process.env.NODE_ENV == "development") {
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
 }
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
-}
+
+const pool = new Pool(config);
+
+// Query function wrapper for logging in development
+const query = async (text, params) => {
+  try {
+    const res = await pool.query(text, params);
+    // if (process.env.NODE_ENV == "development") {
+    //   console.log("executed query", { text });
+    // }
+    return res;
+  } catch (error) {
+    console.error("error in query", { text });
+    throw error;
+  }
+};
+
+module.exports = { query };
